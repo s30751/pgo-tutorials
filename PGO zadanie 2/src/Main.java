828f0c3b0fd56f5751a98744084d86c7af4f461b
Julia Wasilewska <s30751@pjwstk.edu.pl>
2024-03-06 11:41:39 +0100

    Exercise 1

---
+++ PGO zadanie 2/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+  public static void main (String[] args) {
+      int value = 10 ;
+
+      if (value > 0) {
+          System.out.println("The number is positive");
+        } else if (value < 0) {
+          System.out.println("The number is negative");
+      } else {
+          System.out.println("The value equals 0");
+      }
+    }
+}
